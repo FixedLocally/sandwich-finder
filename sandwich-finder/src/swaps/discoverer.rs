f630b4327ae7446045b1bf5a9682cebfe4a543e0
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2025-09-19 02:00:07 +0100

    fix: decrease discoverer false positives

--- sandwich-finder/src/swaps/discoverer.rs
+++ sandwich-finder/src/swaps/discoverer.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 use solana_sdk::{instruction::Instruction, pubkey::Pubkey};
 use yellowstone_grpc_proto::prelude::{InnerInstruction, InnerInstructions, TransactionStatusMeta};
 
-use crate::swaps::{addresses::{DLMM_PUBKEY, METEORA_PUBKEY, PDF2_PUBKEY, PDF_PUBKEY, RAYDIUM_CL_PUBKEY, RAYDIUM_LP_PUBKEY, RAYDIUM_V4_PUBKEY, RAYDIUM_V5_PUBKEY, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID, WHIRLPOOL_PUBKEY}, finder::{SwapFinder, SwapV2}, private::Sealed};
+use crate::swaps::{finder::{SwapFinder, SwapV2}, private::Sealed, utils::token_transferred_inner};
 
 impl Sealed for Discoverer {}
 
@@ -33,42 +33,42 @@ impl SwapFinder for Discoverer {
         )
     }
 
-    fn find_swaps(ix: &Instruction, inner_ixs: &InnerInstructions, account_keys: &Vec<Pubkey>, _meta: &TransactionStatusMeta) -> Vec<SwapV2> {
+    fn find_swaps(ix: &Instruction, inner_ixs: &InnerInstructions, account_keys: &Vec<Pubkey>, meta: &TransactionStatusMeta) -> Vec<SwapV2> {
         // ignore known programs
         match ix.program_id {
             // RAYDIUM_V4_PUBKEY | RAYDIUM_V5_PUBKEY | RAYDIUM_LP_PUBKEY | RAYDIUM_CL_PUBKEY | PDF_PUBKEY | PDF2_PUBKEY | WHIRLPOOL_PUBKEY | DLMM_PUBKEY | METEORA_PUBKEY => vec![],
             _ => {
                 let mut transfer_count = 0;
                 let mut authorities = HashSet::new();
+                let mut mints = HashSet::new();
                 for inner_ix in &inner_ixs.instructions {
-                    let program_id = account_keys[inner_ix.program_id_index as usize];
-                    match program_id {
-                        TOKEN_PROGRAM_ID | TOKEN_2022_PROGRAM_ID => {
-                            if inner_ix.data.len() < 9 {
-                                continue;
-                            }
-                            match inner_ix.data[0] {
-                                3 => { // Transfer
-                                    transfer_count += 1;
-                                    if inner_ix.accounts.len() >= 3 {
-                                        let authority = account_keys[inner_ix.accounts[2] as usize];
-                                        authorities.insert(authority);
-                                    }
-                                },
-                                12 => { // TransferChecked
-                                    transfer_count += 1;
-                                    if inner_ix.accounts.len() >= 4 {
-                                        let authority = account_keys[inner_ix.accounts[3] as usize];
-                                        authorities.insert(authority);
-                                    }
-                                },
-                                _ => {}
-                            }
-                        },
-                        _ => {}
+                    if let Some((_from, _to, mint, _amount)) = token_transferred_inner(&inner_ix, &account_keys, &meta) {
+                        transfer_count += 1;
+                        match inner_ix.data[0] {
+                            2 => { // System transfer
+                                if inner_ix.accounts.len() >= 1 {
+                                    let authority = account_keys[inner_ix.accounts[0] as usize];
+                                    authorities.insert(authority);
+                                }
+                            },
+                            3 => { // Transfer
+                                if inner_ix.accounts.len() >= 3 {
+                                    let authority = account_keys[inner_ix.accounts[2] as usize];
+                                    authorities.insert(authority);
+                                }
+                            },
+                            12 => { // TransferChecked
+                                if inner_ix.accounts.len() >= 4 {
+                                    let authority = account_keys[inner_ix.accounts[3] as usize];
+                                    authorities.insert(authority);
+                                }
+                            },
+                            _ => {}
+                        }
+                        mints.insert(mint);
                     }
                 }
-                if transfer_count >= 2 && authorities.len() >= 2 {
+                if transfer_count >= 2 && authorities.len() >= 2 && mints.len() >= 2 {
                     return vec![
                         SwapV2::new(None, ix.program_id.to_string(), "".to_string(), "".to_string(), "".to_string(), 0, 0, "".to_string(), "".to_string(), 0, 0, 0, 0, None),
                     ];
