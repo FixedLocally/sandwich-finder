6bc5833397fabd01fb908bdc6cab894e6ce6f0bd
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2025-10-10 20:52:46 +0100

    feat: store detections to db

--- sandwich-finder/src/bin/detector.rs
+++ sandwich-finder/src/bin/detector.rs
@@ -1,10 +1,13 @@
-use std::{collections::{HashMap, HashSet}, sync::Arc};
+use std::{collections::{HashMap, HashSet}, sync::{atomic::{AtomicU64, Ordering}, Arc}};
 
 use futures::future::join_all;
-use mysql::{prelude::Queryable, Pool, Row};
-use sandwich_finder::{events::{addresses::is_known_aggregator, common::Timestamp, sandwich::{SandwichCandidate, TradePair}, swap::SwapV2, transaction::TransactionV2, transfer::TransferV2}, utils::create_db_pool};
+use mysql::{prelude::Queryable, Pool, Row, Value};
+use sandwich_finder::{events::{common::Timestamp, sandwich::detect, swap::SwapV2, transaction::TransactionV2, transfer::TransferV2}, utils::create_db_pool};
 use serde::{Deserialize, Serialize};
-use solana_sdk::pubkey::Pubkey;
+use uuid::Uuid;
+
+const MAX_CHUNK_SIZE: u64 = 1000; // max slots to fetch at a time
+const LEADER_GROUP_SIZE: u64 = 4; // slots per leader group
 
 #[derive(Debug, Serialize, Deserialize)]
 struct GraphNode {
@@ -37,11 +40,12 @@ struct TransferGraph {
 
 async fn get_events(conn: Pool, start_slot: u64, end_slot: u64) -> (Vec<SwapV2>, Vec<TransferV2>, Vec<TransactionV2>) {
     let conn = &mut conn.get_conn().unwrap();
-    let res: Vec<Row> = conn.exec("select event_type, slot, inclusion_order, ix_index, inner_ix_index, authority, outer_program, program, amm, input_mint, output_mint, input_amount, output_amount, input_ata, output_ata, input_inner_ix_index, output_inner_ix_index from events where slot between ? and ?", vec![start_slot, end_slot]).unwrap();
+    let res: Vec<Row> = conn.exec("select id, event_type, slot, inclusion_order, ix_index, inner_ix_index, authority, outer_program, program, amm, input_mint, output_mint, input_amount, output_amount, input_ata, output_ata, input_inner_ix_index, output_inner_ix_index from events where slot between ? and ?", vec![start_slot, end_slot]).unwrap();
     let mut swaps = vec![];
     let mut transfers = vec![];
     let mut txs = vec![];
     for row in res {
+        let id: u64 = row.get("id").unwrap();
         let event_type: Arc<str> = row.get("event_type").unwrap();
         let slot: u64 = row.get("slot").unwrap();
         let inclusion_order: u32 = row.get("inclusion_order").unwrap();
@@ -64,10 +68,10 @@ async fn get_events(conn: Pool, start_slot: u64, end_slot: u64) -> (Vec<SwapV2>,
         let output_inner_ix_index = output_inner_ix_index.filter(|&x| x >= 0).map(|x| x as u32);
         match event_type.as_ref() {
             "SWAP" => {
-                swaps.push(SwapV2::new(outer_program, program, authority, amm.unwrap(), input_mint, output_mint, input_amount, output_amount, input_ata, output_ata, input_inner_ix_index, output_inner_ix_index, slot, inclusion_order, ix_index, inner_ix_index));
+                swaps.push(SwapV2::new(outer_program, program, authority, amm.unwrap(), input_mint, output_mint, input_amount, output_amount, input_ata, output_ata, input_inner_ix_index, output_inner_ix_index, slot, inclusion_order, ix_index, inner_ix_index, id));
             },
             "TRANSFER" => {
-                transfers.push(TransferV2::new(outer_program, program, authority, input_mint, input_amount, input_ata, output_ata, slot, inclusion_order, ix_index, inner_ix_index));
+                transfers.push(TransferV2::new(outer_program, program, authority, input_mint, input_amount, input_ata, output_ata, slot, inclusion_order, ix_index, inner_ix_index, id));
             },
             _ => {},
         }
@@ -108,91 +112,6 @@ async fn get_events(conn: Pool, start_slot: u64, end_slot: u64) -> (Vec<SwapV2>,
     (swaps, transfers, txs)
 }
 
-/// This function expects the events to be sorted in chronological order (which [get_event] does)
-fn detect_main(swaps: &[SwapV2], transfers: &[TransferV2], txs: &[TransactionV2]) {
-    // Group swaps by AMM then direction also by outer program
-    let mut amm_swaps: HashMap<Arc<str>, HashMap<TradePair, Vec<SwapV2>>> = HashMap::new();
-    for swap in swaps.iter() {
-        let pair = TradePair::new(
-            swap.amm().clone(),
-            swap.input_mint().clone(),
-            swap.output_mint().clone(),
-        );
-        amm_swaps.entry(swap.amm().clone()).or_default().entry(pair.clone()).or_default().push(swap.clone());
-    }
-
-    // for each swap, we want to match it with a series of swaps before it in the same direction and a series of swaps after it in the opposite direction
-    let mut matched_timestamps = HashSet::new(); // to avoid double counting
-    for swap in swaps.iter() {
-        if matched_timestamps.contains(swap.timestamp()) {
-            continue;
-        }
-        let pair = TradePair::new(
-            swap.amm().clone(),
-            swap.input_mint().clone(),
-            swap.output_mint().clone(),
-        );
-        let rev_pair = pair.reverse();
-        let before_swaps = amm_swaps.get(swap.amm()).and_then(|m| m.get(&pair)).map(|v| v.iter().filter(|s| s.timestamp() < swap.timestamp()).cloned().collect::<Vec<_>>()).unwrap_or_default();
-        let after_swaps = amm_swaps.get(swap.amm()).and_then(|m| m.get(&rev_pair)).map(|v| v.iter().filter(|s| s.timestamp() > swap.timestamp()).cloned().collect::<Vec<_>>()).unwrap_or_default();
-        if before_swaps.is_empty() || after_swaps.is_empty() {
-            continue;
-        }
-        // println!("Analyzing swap at {:?} for sandwiches {:?} {:?}", swap.timestamp(), before_swaps, after_swaps);
-        // we then group the swaps before and after by outer program and see if some outer program may be sandwiching this swap
-        let before_outer = {
-            let mut map: HashMap<Option<Arc<str>>, Vec<SwapV2>> = HashMap::new();
-            for s in before_swaps.iter() {
-                map.entry(s.outer_program().clone()).or_default().push(s.clone());
-            }
-            map
-        };
-        let after_outer = {
-            let mut map: HashMap<Option<Arc<str>>, Vec<SwapV2>> = HashMap::new();
-            for s in after_swaps.iter() {
-                map.entry(s.outer_program().clone()).or_default().push(s.clone());
-            }
-            map
-        };
-        let mut candidates = vec![];
-        for (k, before_swaps) in before_outer.iter() {
-            if k.is_some() && is_known_aggregator(&Pubkey::from_str_const(k.as_ref().unwrap())) {
-                continue;
-            }
-            if let Some(after_swaps) = after_outer.get(k) {
-                // loop thru all possible contiguous segments of before_swaps and after_swaps and try to contruct a sandwich out of them
-                // println!("Looking at outer program {:?} {} {}", k, before_swaps.len(), after_swaps.len());
-                for i in 0..before_swaps.len() {
-                    for j in i+1..=before_swaps.len() {
-                        for m in 0..after_swaps.len() {
-                            for n in m+1..=after_swaps.len() {
-                                let frontrun = &before_swaps[i..j];
-                                let frontrun_last = before_swaps[j - 1].clone();
-                                let backrun = &after_swaps[m..n];
-                                let backrun_first = after_swaps[m].clone();
-                                let victim = &swaps.iter().filter(|s| s.timestamp() > frontrun_last.timestamp() && s.timestamp() < backrun_first.timestamp() && s.amm() == swap.amm() && s.input_mint() == swap.input_mint() && s.output_mint() == swap.output_mint()).cloned().collect::<Vec<_>>()[..];
-                                match SandwichCandidate::new(frontrun, victim, backrun, &transfers, &txs) {
-                                    Ok(sandwich) => {
-                                        candidates.push(sandwich);
-                                        victim.iter().for_each(|s| { matched_timestamps.insert(*s.timestamp()); });
-                                    }
-                                    // Err(e) => println!("Failed to create sandwich candidate: {},{},{},{} {:?}", i,j,m,n,e),
-                                    Err(_) => {},
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        // if there are multiple candidates, we pick the one with the most victims, then the one with the most swaps
-        if !candidates.is_empty() {
-            candidates.sort_by_cached_key(|c| (c.victim().len(), c.frontrun().len() + c.backrun().len()));
-            let sandwich = candidates.last().unwrap();
-            println!("Found sandwich {:?}", sandwich);
-        }
-    }
-}
 // Swap in slot 371237175 (order 1242, ix 1, inner_ix Some(1))
 // Swap in slot 371237175 (order 1247, ix 5, inner_ix None)
 // Swap in slot 371237175 (order 1248, ix 2, inner_ix Some(0))
@@ -214,35 +133,72 @@ async fn main() {
         start_slot
     };
     // alignment
-    let start_slot = start_slot / 4 * 4;
-    let end_slot = end_slot / 4 * 4 + 3;
-    // fetch events for 1k slots at a time and process in groups of 4 slots
+    let start_slot = start_slot / LEADER_GROUP_SIZE * LEADER_GROUP_SIZE;
+    let end_slot = end_slot / LEADER_GROUP_SIZE * LEADER_GROUP_SIZE + LEADER_GROUP_SIZE - 1;
+    // fetch events for up to 1k slots at a time and process in groups of 4 slots
     let pool = create_db_pool();
-    let handles: Vec<_> = (start_slot..=end_slot).step_by(1000).map(|chunk_start| {
-        let chunk_end = (chunk_start + 999).min(end_slot);
+    let chunk_size = ((end_slot - start_slot + 1) / 16).min(MAX_CHUNK_SIZE - LEADER_GROUP_SIZE) / LEADER_GROUP_SIZE * LEADER_GROUP_SIZE + LEADER_GROUP_SIZE;
+    println!("Processing slots {} to {} ({} leader groups)", start_slot, end_slot, (end_slot - start_slot + 1) / LEADER_GROUP_SIZE);
+    let progress = Arc::from(AtomicU64::new(0));
+    let handles: Vec<_> = (start_slot..=end_slot).step_by(chunk_size as usize).map(|chunk_start| {
+        let chunk_end = (chunk_start + chunk_size - 1).min(end_slot);
         let pool = pool.clone(); // docs said this is cloneable
+        let progress = progress.clone();
         tokio::spawn(async move {
-            let (swaps, transfers, txs) = get_events(pool, chunk_start, chunk_end).await;
+            // println!("Fetching events for slots {} to {}", chunk_start, chunk_end);
+            let (swaps, transfers, txs) = get_events(pool.clone(), chunk_start, chunk_end).await;
             let mut swaps_start = 0;
             let mut transfers_start = 0;
             let mut txs_start = 0;
+            let mut conn = pool.get_conn().unwrap();
             for slot in (chunk_start..=chunk_end).step_by(4) {
-                let swaps_end = swaps.iter().skip(swaps_start).position(|s| *s.slot() >= slot + 4).map(|n| n + swaps_start).unwrap_or(swaps.len());
-                let transfers_end = transfers.iter().skip(transfers_start).position(|t| *t.slot() >= slot + 4).map(|n| n + transfers_start).unwrap_or(transfers.len());
-                let txs_end = txs.iter().skip(txs_start).position(|t| *t.slot() >= slot + 4).map(|n| n + txs_start).unwrap_or(txs.len());
+                let swaps_end = swaps.iter().skip(swaps_start).position(|s| *s.slot() >= slot + LEADER_GROUP_SIZE).map(|n| n + swaps_start).unwrap_or(swaps.len());
+                let transfers_end = transfers.iter().skip(transfers_start).position(|t| *t.slot() >= slot + LEADER_GROUP_SIZE).map(|n| n + transfers_start).unwrap_or(transfers.len());
+                let txs_end = txs.iter().skip(txs_start).position(|t| *t.slot() >= slot + LEADER_GROUP_SIZE).map(|n| n + txs_start).unwrap_or(txs.len());
 
                 let slot_swaps = &swaps[swaps_start..swaps_end];
                 let slot_transfers = &transfers[transfers_start..transfers_end];
                 let slot_txs = &txs[txs_start..txs_end];
-                println!("Processing slots {} to {}", slot, slot + 3);
-                println!("Swaps: {:#?}", slot_swaps.len());
-                println!("Transfers: {:#?}", slot_transfers.len());
-                println!("Txs: {:#?}", slot_txs.len());
-                detect_main(slot_swaps, slot_transfers, slot_txs);
+                // println!("Processing slots {} to {}", slot, slot + LEADER_GROUP_SIZE - 1);
+                // println!("Swaps: {:#?}", slot_swaps.len());
+                // println!("Transfers: {:#?}", slot_transfers.len());
+                // println!("Txs: {:#?}", slot_txs.len());
+                let sandwiches = detect(slot_swaps, slot_transfers, slot_txs);
+                // for sandwich in sandwiches.iter() {
+                //     println!("Detected sandwich: {:#?}", sandwich);
+                // }
+
+                let args: Vec<_> = sandwiches.iter().flat_map(|s| {
+                    // deterministic id for each sandwich
+                    let name: Vec<u8> = sandwiches.iter().flat_map(|s| {
+                        [
+                            s.frontrun().iter().flat_map(|sw| sw.id().to_le_bytes()).collect::<Vec<_>>(),
+                            s.backrun().iter().flat_map(|sw| sw.id().to_le_bytes()).collect::<Vec<_>>(),
+                            s.victim().iter().flat_map(|sw| sw.id().to_le_bytes()).collect::<Vec<_>>(),
+                            s.transfers().iter().flat_map(|sw| sw.id().to_le_bytes()).collect::<Vec<_>>(),
+                        ].concat()
+                    }).collect();
+                    let uuid = &*Uuid::new_v5(&Uuid::NAMESPACE_DNS, &name).to_string();
+                    [
+                        s.frontrun().iter().flat_map(|sw| vec![Value::from(uuid), Value::from(sw.id()), Value::from("FRONTRUN")]).collect::<Vec<_>>(),
+                        s.backrun().iter().flat_map(|sw| vec![Value::from(uuid), Value::from(sw.id()), Value::from("BACKRUN")]).collect::<Vec<_>>(),
+                        s.victim().iter().flat_map(|sw| vec![Value::from(uuid), Value::from(sw.id()), Value::from("VICTIM")]).collect::<Vec<_>>(),
+                        s.transfers().iter().flat_map(|sw| vec![Value::from(uuid), Value::from(sw.id()), Value::from("TRANSFER")]).collect::<Vec<_>>(),
+                    ].concat()
+                }).collect();
+                if !args.is_empty() {
+                    let stmt = format!("insert into sandwiches (id, event_id, role) values {}", "(?, ?, ?),".repeat(args.len() / 3));
+                    let stmt = stmt.trim_end_matches(",").to_string();
+                    conn.exec_drop(stmt, args).unwrap();
+                }
 
                 swaps_start = swaps_end;
                 transfers_start = transfers_end;
                 txs_start = txs_end;
+                let completed = progress.fetch_add(1, Ordering::AcqRel);
+                if completed % 100 == 0 {
+                    println!("{}/{}", completed, (end_slot - start_slot + 1) / LEADER_GROUP_SIZE);
+                }
             }
         })
     }).collect();
--- sandwich-finder/src/events/sandwich.rs
+++ sandwich-finder/src/events/sandwich.rs
@@ -1,9 +1,10 @@
-use std::{collections::HashSet, sync::Arc};
+use std::{collections::{HashMap, HashSet}, sync::Arc};
 
 use derive_getters::Getters;
+use solana_sdk::pubkey::Pubkey;
 use thiserror::Error;
 
-use crate::events::{swap::SwapV2, transaction::TransactionV2, transfer::TransferV2};
+use crate::events::{addresses::is_known_aggregator, swap::SwapV2, transaction::TransactionV2, transfer::TransferV2};
 
 #[derive(Debug, Error)]
 pub enum SandwichError {
@@ -64,7 +65,7 @@ impl TradePair {
 /// To reduce false positives, steps 1 and 5 must use the same non null non well-known aggregator outer program,
 /// the justification being well-known aggregators aren't designed for sandwichers to keep track of their tokens across txs.
 /// Victim swaps also can't use the same wrapper program as the frontrun/backrun swaps.
-#[derive(Debug, Getters)]
+#[derive(Clone, Debug, Getters)]
 pub struct SandwichCandidate {
     frontrun: Arc<[SwapV2]>,
     victim: Arc<[SwapV2]>,
@@ -143,3 +144,129 @@ impl SandwichCandidate {
         })
     }
 }
+
+/// This function expects the events to be sorted in chronological order
+pub fn detect(swaps: &[SwapV2], transfers: &[TransferV2], txs: &[TransactionV2]) -> Arc<[SandwichCandidate]> {
+    // Group swaps by AMM then direction also by outer program
+    let mut amm_swaps: HashMap<Arc<str>, HashMap<TradePair, Vec<SwapV2>>> = HashMap::new();
+    for swap in swaps.iter() {
+        let pair = TradePair::new(
+            swap.amm().clone(),
+            swap.input_mint().clone(),
+            swap.output_mint().clone(),
+        );
+        amm_swaps.entry(swap.amm().clone()).or_default().entry(pair.clone()).or_default().push(swap.clone());
+    }
+
+    // for each swap, we want to match it with a series of swaps before it in the same direction and a series of swaps after it in the opposite direction
+    let mut matched_timestamps = HashSet::new(); // to avoid double counting
+    let mut sandwiches = vec![];
+    for swap in swaps.iter() {
+        if matched_timestamps.contains(swap.timestamp()) {
+            continue;
+        }
+        let pair = TradePair::new(
+            swap.amm().clone(),
+            swap.input_mint().clone(),
+            swap.output_mint().clone(),
+        );
+        let rev_pair = pair.reverse();
+        let before_swaps = amm_swaps.get(swap.amm()).and_then(|m| m.get(&pair)).map(|v| v.iter().filter(|s| s.timestamp() < swap.timestamp()).cloned().collect::<Vec<_>>()).unwrap_or_default();
+        let after_swaps = amm_swaps.get(swap.amm()).and_then(|m| m.get(&rev_pair)).map(|v| v.iter().filter(|s| s.timestamp() > swap.timestamp()).cloned().collect::<Vec<_>>()).unwrap_or_default();
+        if before_swaps.is_empty() || after_swaps.is_empty() {
+            continue;
+        }
+        // println!("Analyzing swap at {:?} for sandwiches {:?} {:?}", swap.timestamp(), before_swaps, after_swaps);
+        // we then group the swaps before and after by outer program and see if some outer program may be sandwiching this swap
+        let before_outer = {
+            let mut map: HashMap<Option<Arc<str>>, Vec<SwapV2>> = HashMap::new();
+            for s in before_swaps.iter() {
+                map.entry(s.outer_program().clone()).or_default().push(s.clone());
+            }
+            map
+        };
+        let after_outer = {
+            let mut map: HashMap<Option<Arc<str>>, Vec<SwapV2>> = HashMap::new();
+            for s in after_swaps.iter() {
+                map.entry(s.outer_program().clone()).or_default().push(s.clone());
+            }
+            map
+        };
+        let mut candidates = vec![];
+        for (k, before_swaps) in before_outer.iter() {
+            if k.is_some() && is_known_aggregator(&Pubkey::from_str_const(k.as_ref().unwrap())) {
+                continue;
+            }
+            if let Some(after_swaps) = after_outer.get(k) {
+                // loop thru all possible contiguous segments of before_swaps and after_swaps and try to contruct a sandwich out of them
+                // println!("Looking at outer program {:?} {} {}", k, before_swaps.len(), after_swaps.len());
+                for i in 0..before_swaps.len() {
+                    for j in i+1..=before_swaps.len() {
+                        for m in 0..after_swaps.len() {
+                            for n in m+1..=after_swaps.len() {
+                                let frontrun = &before_swaps[i..j];
+                                let frontrun_last = before_swaps[j - 1].clone();
+                                let backrun = &after_swaps[m..n];
+                                let backrun_first = after_swaps[m].clone();
+                                let victim = &swaps.iter().filter(|s| s.timestamp() > frontrun_last.timestamp() && s.timestamp() < backrun_first.timestamp() && s.amm() == swap.amm() && s.input_mint() == swap.input_mint() && s.output_mint() == swap.output_mint()).cloned().collect::<Vec<_>>()[..];
+                                match SandwichCandidate::new(frontrun, victim, backrun, &transfers, &txs) {
+                                    Ok(sandwich) => {
+                                        candidates.push(sandwich);
+                                        victim.iter().for_each(|s| { matched_timestamps.insert(*s.timestamp()); });
+                                    }
+                                    // Err(e) => println!("Failed to create sandwich candidate: {},{},{},{} {:?}", i,j,m,n,e),
+                                    Err(_) => {},
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // if there are multiple candidates, we pick the one with the most victims, then the one with the most swaps
+        if !candidates.is_empty() {
+            candidates.sort_by_cached_key(|c| (c.victim().len(), c.frontrun().len() + c.backrun().len()));
+            sandwiches.push(candidates.last().unwrap().clone());
+        }
+    }
+
+    sandwiches.into()
+}
+/*
+SandwichCandidate {
+  frontrun: [
+    Swap in slot 372367924 (order 1121, ix 5, inner_ix None)
+      on dbcij3LWUppWqq96dh6gJWwBifmcGfLSB5D4DuSMaqN market 8QhobkasSgim5hxmUgF7xEvm9piz4KyBp4ATNPYRusHa
+      Route So11111111111111111111111111111111111111112 -> GUCwGWVATG5jZxagJWwhJgFasV2XH7DgqF9gJgveV3oJ Amounts 31432000 -> 27238764881
+      ATAs Ha4yHUP5P9ye8J2nuWDuckYvsK8RAsXXqxkBjm4dfm7V -> F6hLzeLQ4vnrdEnikXvUcRfNF2E4zWFxVKfTfanSHNVu
+  ],
+  victim: [
+    Swap in slot 372367925 (order 1136, ix 5, inner_ix None)
+      on dbcij3LWUppWqq96dh6gJWwBifmcGfLSB5D4DuSMaqN market 8QhobkasSgim5hxmUgF7xEvm9piz4KyBp4ATNPYRusHa
+      Route So11111111111111111111111111111111111111112 -> GUCwGWVATG5jZxagJWwhJgFasV2XH7DgqF9gJgveV3oJ Amounts 1418439693 -> 1262993306810
+      ATAs 6kMydTUPUK9ntXyAdSypft9es4mqH2CaxjoYKcCuqfS9 -> F9Kzc4LG5XQsmY8JrimnKU1TA16jednkvnDEmH2Xj2d8,
+    Swap in slot 372367925 (order 1147, ix 5, inner_ix None)
+      on dbcij3LWUppWqq96dh6gJWwBifmcGfLSB5D4DuSMaqN market 8QhobkasSgim5hxmUgF7xEvm9piz4KyBp4ATNPYRusHa
+      Route So11111111111111111111111111111111111111112 -> GUCwGWVATG5jZxagJWwhJgFasV2XH7DgqF9gJgveV3oJ Amounts 1406197017 -> 1206419070141
+      ATAs 6TALPjXACbQnhHHWoCJUypHVM7AqXzmHdnXb2nm4sh7u -> Kk6tDPgYpGnKY7c4yBidPywJKBr4XHjmnqyuwA1BtCN,
+    Swap in slot 372367926 (order 1101, ix 5, inner_ix None)
+      on dbcij3LWUppWqq96dh6gJWwBifmcGfLSB5D4DuSMaqN market 8QhobkasSgim5hxmUgF7xEvm9piz4KyBp4ATNPYRusHa
+      Route So11111111111111111111111111111111111111112 -> GUCwGWVATG5jZxagJWwhJgFasV2XH7DgqF9gJgveV3oJ Amounts 1436087902 -> 1249015798317
+      ATAs HknctZFeNjkwdSawyj85yBAtaxSJAscVNKBpZwZo9beY -> 2VWFWgk2e9bemJvfLDoKQNtt53bPfGa6puhmCb82YoSU
+  ],
+  backrun: [
+    Swap in slot 372367926 (order 1326, ix 3, inner_ix None)
+      on dbcij3LWUppWqq96dh6gJWwBifmcGfLSB5D4DuSMaqN market 8QhobkasSgim5hxmUgF7xEvm9piz4KyBp4ATNPYRusHa
+      Route GUCwGWVATG5jZxagJWwhJgFasV2XH7DgqF9gJgveV3oJ -> So11111111111111111111111111111111111111112 Amounts 27238764881 -> 31894629
+      ATAs F6hLzeLQ4vnrdEnikXvUcRfNF2E4zWFxVKfTfanSHNVu -> Ha4yHUP5P9ye8J2nuWDuckYvsK8RAsXXqxkBjm4dfm7V
+  ],
+  transfers: [],
+  txs: [
+    TransactionV2 { slot: 372367924, inclusion_order: 1121, sig: "3p4cmmRUfEL3GFEtaTNgifNmDpF6fpoxvgM2p5Pf32rzFyu2BpM3qEy8ZoghfURgvgg3n45ZN978s3voyAX8qVYb", fee: 6200, cu_actual: 75297 },
+    TransactionV2 { slot: 372367925, inclusion_order: 1136, sig: "28pQvdt1xrFTxk2G15WQZu8aVtPdQzsDEVyXBjuKk22n2X6zn7pFkeDzfjvJVN9jSBVVkcVkMj17TvnTxmjpac2B", fee: 6200, cu_actual: 76791 },
+    TransactionV2 { slot: 372367925, inclusion_order: 1147, sig: "42RwgPEvRc2rwTixBrRKKfuBUKh8k6QaW4KKyyds44bseyGxkTnkvqrqzdaiXUsgGHtQVZXBGkBdaB81KakCHyXD", fee: 6200, cu_actual: 76797 },
+    TransactionV2 { slot: 372367926, inclusion_order: 1101, sig: "3Jb6uRtCtwTpvk5wndPvR4zmyxMgskhoDAHkgmAcmBpJiQfjecAE23DhuoM1iZ7ujcAAi5bneZiN5DFFEJa7s1Br", fee: 6200, cu_actual: 75294 },
+    TransactionV2 { slot: 372367926, inclusion_order: 1326, sig: "LP29LGbuePvufokqmMW2yVvQGX48PA6D32XDX4MPKhdGrpvDuhDZBXVSkmM4XX8xMPEKFNGqWaukfk1g6GQgevq", fee: 6200, cu_actual: 69165 }
+  ]
+}
+ */
