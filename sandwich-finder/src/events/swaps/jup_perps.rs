ef22f0e863e3c1c57589060d2e050a539011063a
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2025-10-19 20:05:23 +0100

    feat: another jup perps ix

--- sandwich-finder/src/events/swaps/jup_perps.rs
+++ sandwich-finder/src/events/swaps/jup_perps.rs
@@ -3,13 +3,34 @@ use yellowstone_grpc_proto::prelude::{InnerInstruction, InnerInstructions, Trans
 
 use crate::events::{addresses::JUP_PERPS_PUBKEY, swap::{SwapFinder, SwapV2}, swaps::{private::Sealed, swap_finder_ext::SwapFinderExt}};
 
+enum JupPerpsSwapVariant {
+    Swap2,
+    SwapWithTokenLedger,
+    InstantIncreasePositionPreSwap,
+}
+
 impl Sealed for JupPerpsSwapFinder {}
 
 pub struct JupPerpsSwapFinder {}
 
+impl JupPerpsSwapFinder {
+    fn variant_from_data(data: &[u8]) -> Option<JupPerpsSwapVariant> {
+        if data.starts_with(&[0x41, 0x4b, 0x3f, 0x4c, 0xeb, 0x5b, 0x5b, 0x88]) {
+            Some(JupPerpsSwapVariant::Swap2)
+        } else if data.starts_with(&[0x8b, 0x8d, 0xee, 0xc5, 0x29, 0xd3, 0xac, 0x13]) {
+            Some(JupPerpsSwapVariant::SwapWithTokenLedger)
+        } else if data.starts_with(&[0xc5, 0x26, 0x56, 0xa5, 0xc7, 0x17, 0x26, 0xea]) {
+            Some(JupPerpsSwapVariant::InstantIncreasePositionPreSwap)
+        } else {
+            None
+        }
+    }
+}
+
 /// Jup perps swaps have two variants:
 /// 1. swap2 [0x41, 0x4b, 0x3f, 0x4c, 0xeb, 0x5b, 0x5b, 0x88]
 /// 2. swapWithTokenLedger [0x8b, 0x8d, 0xee, 0xc5, 0x29, 0xd3, 0xac, 0x13]
+/// 3. instantIncreasePositionPreSwap [0xc5, 0x26, 0x56, 0xa5, 0xc7, 0x17, 0x26, 0xea]
 /// In/min out amounts follows the discriminant
 impl SwapFinder for JupPerpsSwapFinder {
     fn amm_ix(ix: &Instruction) -> Pubkey {
@@ -35,17 +56,31 @@ impl SwapFinder for JupPerpsSwapFinder {
     }
     
     fn pool_ata_ix(ix: &Instruction) -> (Pubkey, Pubkey) {
-        (
-            ix.accounts[13].pubkey,
-            ix.accounts[9].pubkey,
-        )
+        match Self::variant_from_data(&ix.data) {
+            Some(JupPerpsSwapVariant::Swap2) | Some(JupPerpsSwapVariant::SwapWithTokenLedger) => (
+                ix.accounts[13].pubkey,
+                ix.accounts[9].pubkey,
+            ),
+            Some(JupPerpsSwapVariant::InstantIncreasePositionPreSwap) => (
+                ix.accounts[11].pubkey,
+                ix.accounts[8].pubkey,
+            ),
+            None => unreachable!(),
+        }
     }
     
     fn pool_ata_inner_ix(inner_ix: &InnerInstruction, account_keys: &Vec<Pubkey>) -> (Pubkey, Pubkey) {
-        (
-            account_keys[inner_ix.accounts[13] as usize],
-            account_keys[inner_ix.accounts[9] as usize],
-        )
+        match Self::variant_from_data(&inner_ix.data) {
+            Some(JupPerpsSwapVariant::Swap2) | Some(JupPerpsSwapVariant::SwapWithTokenLedger) => (
+                account_keys[inner_ix.accounts[13] as usize],
+                account_keys[inner_ix.accounts[9] as usize],
+            ),
+            Some(JupPerpsSwapVariant::InstantIncreasePositionPreSwap) => (
+                account_keys[inner_ix.accounts[11] as usize],
+                account_keys[inner_ix.accounts[8] as usize],
+            ),
+            _ => unreachable!(),
+        }
     }
 
     fn find_swaps(ix: &Instruction, inner_ixs: &InnerInstructions, account_keys: &Vec<Pubkey>, meta: &TransactionStatusMeta) -> Vec<SwapV2> {
@@ -54,6 +89,8 @@ impl SwapFinder for JupPerpsSwapFinder {
             Self::find_swaps_generic(ix, inner_ixs, account_keys, meta, &JUP_PERPS_PUBKEY, &[0x41, 0x4b, 0x3f, 0x4c, 0xeb, 0x5b, 0x5b, 0x88], 0, 24),
             // swap_base_output
             Self::find_swaps_generic(ix, inner_ixs, account_keys, meta, &JUP_PERPS_PUBKEY, &[0x8b, 0x8d, 0xee, 0xc5, 0x29, 0xd3, 0xac, 0x13], 0, 24),
+            // instant_increase_position_pre_swap
+            Self::find_swaps_generic(ix, inner_ixs, account_keys, meta, &JUP_PERPS_PUBKEY, &[0xc5, 0x26, 0x56, 0xa5, 0xc7, 0x17, 0x26, 0xea], 0, 24),
         ].concat()
     }
 }
