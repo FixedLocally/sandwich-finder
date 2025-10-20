944ba6c08760e1fc4f560373150870e8af54b0e4
FixedLocally <9797160+FixedLocally@users.noreply.github.com>
2025-10-20 01:08:07 +0100

    fix: blacklist more irrelevant combos

--- sandwich-finder/src/events/swaps/discoverer.rs
+++ sandwich-finder/src/events/swaps/discoverer.rs
@@ -10,6 +10,11 @@ const BLACKLISTED_COMBINATIONS: &[(Pubkey, &[u8], usize)] = &[ // program, discr
     (Pubkey::from_str_const("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"), &[], 0), // metaplex
     (Pubkey::from_str_const("Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB"), &[0xa9, 0x20, 0x4f, 0x89, 0x88, 0xe8, 0x46, 0x89], 0), // meteora claim fees
     (Pubkey::from_str_const("dbcij3LWUppWqq96dh6gJWwBifmcGfLSB5D4DuSMaqN"), &[0x9c, 0xa9, 0xe6, 0x67, 0x35, 0xe4, 0x50, 0x40], 0), // dbc migrate
+    (Pubkey::from_str_const("mmm3XBJg5gk8XJxEKBvdgptZz6SgK4tXvn36sodowMc"), &[], 0), // metaplex mmm, nft trades that we aren't interested in
+    (Pubkey::from_str_const("M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"), &[], 0), // magic eden
+    (Pubkey::from_str_const("APR1MEny25pKupwn72oVqMH4qpDouArsX8zX4VwwfoXD"), &[], 0), // star atlas stuff
+    (Pubkey::from_str_const("SAGE2HAwep459SNq61LHvjxPk4pLPEJLoMETef7f7EE"), &[], 0), // star atlas stuff
+    (Pubkey::from_str_const("Cargo2VNTPPTi9c1vq1Jw5d3BWUNr18MjRtSupAghKEk"), &[], 0), // star atlas stuff
 ];
 
 impl Sealed for Discoverer {}
